c1df83a048d036fe12c06bfdce7bedd248011ce4
prajulmp <150608865+prajulmp@users.noreply.github.com>
2023-11-13 13:08:55 +0100

    Add files via upload

--- math_quiz.py
+++ math_quiz.py
@@ -1,46 +1,45 @@
 import random
 
-
-def function_A(min, max):
+def generate_random_integer(min_value, max_value): #Selecting a random integer for the quiz
     """
-    Random integer.
+    Generate a random integer between min_value and max_value.
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value) #returing the random integer
 
+def choose_random_operator(): #selecting a random math operator
+    return random.choice(['+', '-', '*']) #returning a random math function
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def calculate_expression(num1, num2, operator):
+    expression = f"{num1} {operator} {num2}"
+    if operator == '+': result = num1 + num2 #Addition
+    elif operator == '-': result = num1 - num2 #Sutraction
+    else: result = num1 * num2 #Multiplication
+    return expression, result
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
-
-def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+def run_math_quiz():
+    score = 0
+    total_questions = 3 #total question numbers 
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problems, and you need to give the correct answers for the following problem to test your math skills.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions): #run loop for total questions
+        num1 = generate_random_integer(1, 10) #function to select the random number
+        num2 = generate_random_integer(1, 5) #function to select the random number
+        operator = choose_random_operator() #function to select the random operator 
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = calculate_expression(num1, num2, operator)
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        user_answer = int(user_answer)
 
-        if useranswer == ANSWER:
+        if user_answer == answer: #Checks the answer is right or wrong
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}") #printing the score
 
 if __name__ == "__main__":
-    math_quiz()
+    run_math_quiz()
